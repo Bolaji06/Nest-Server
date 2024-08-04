c276e64007e82d91b9e6588e073b14f0599bf728
Bolaji06 <joshspencer0603@gmail.com>
2024-08-04 05:58:16 +0100

    implement delete post api endpoint

--- routes/save_post.js
+++ routes/save_post.js
@@ -1,13 +1,14 @@
 
 import express from "express";
 import { userAuthorization } from "../utils/authorization.js";
-import { addSavedPost, getAllSavedPost, getSavedPost } from "../controllers/save_post.controller.js";
+import { addSavedPost, getAllSavedPost, getSavedPost, removeSavedPost } from "../controllers/save_post.controller.js";
 
 const router = express.Router();
 
 router.get('/', userAuthorization, getAllSavedPost);
 router.get("/:id", userAuthorization, getSavedPost);
 router.post("/", userAuthorization, addSavedPost);
+router.delete("/", userAuthorization, removeSavedPost);
 
 
 export default router
