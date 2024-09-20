010bb2f9badf12b54d404e73b230f5dba4df507c
Bolaji06 <joshspencer0603@gmail.com>
2024-09-20 23:33:02 +0100

    re-writting the chat features

--- index.js
+++ index.js
@@ -11,18 +11,22 @@ import email from "./routes/email.js";
 import express from "express";
 import timeout from "connect-timeout";
 import cors from "cors";
-import http from 'http';
+import http from "http";
 
 import cookieParser from "cookie-parser";
 import { WebSocketServer } from "ws";
+import prisma from "./lib/prisma.js";
+import { parse } from "path";
+import { stat } from "fs";
 import { error } from "console";
+import { findOrCreateChat, getAllMessages } from "./utils/chat.js";
 
 const PORT = 7000;
 const app = express();
 const allowedOrigins = [process.env.DEV_CORS_URL, process.env.PROD_CORS_URL];
 
 const server = http.createServer(app);
-const wss = new WebSocketServer({ server })
+const wss = new WebSocketServer({ server });
 
 //app.use(timeout("10s"));
 app.use(cookieParser(process.env.TOKEN_SECRET));
@@ -53,6 +57,32 @@ app.get("/", (req, res) => {
   res.send("Welcome to the homepage");
 });
 
+let usersChat;
+// create websocket connection
+wss.on("connection", async (ws) => {
+  // handle any websocket errors
+  let mText = "";
+  console.log("connected...");
+
+  // receive the message from client
+  ws.on("message", async (message) => {
+    if (message) {
+      usersChat = JSON.parse(message.toString());
+      console.log(usersChat);
+    }
+    if (usersChat) {
+      const { userId, receiverId, text } = usersChat;
+      // find or create new chat
+      const chat = await findOrCreateChat(userId, receiverId, text);
+      if (chat) {
+        // get all messages from chat
+        const messages = await getAllMessages(chat.chatId);
+        ws.send(JSON.stringify(messages));
+      }
+    }
+  });
+});
+
 function haltRequestOnTimeout(req, res, next) {
   if (!req.timedout) return next();
   else {
@@ -66,34 +96,6 @@ app.use((err, req, res, next) => {
   return res.status(503).json({ success: false, message: "request timeout" });
   //.send("An unexpected error occurred. Please try again later.");
 });
-
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-
- ws.on('message', (message) => {
-  const decodeMessage = message.toString();
-  console.log('Receiving message ', decodeMessage);
-  
-  wss.clients.forEach((client) => {
-    if (client.readyState === ws.OPEN){
-      client.send(decodeMessage);
-    }
-  })
- })
-
- ws.on("error", (error) => {
-  console.error('Client error ', error);
- })
-
- ws.on('close', () => {
-  console.log('Client disconnected')
- })
-})
-
-wss.on("error", (err) => {
-  console.log("WebSocket server error ", error);
-})
-
 
 server.listen(PORT, () => {
   console.log("Server starting at " + PORT);
